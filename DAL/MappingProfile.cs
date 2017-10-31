101a2be95afa6b53fbd876f12e7dccc05cb1b576
pelle <pelle@flork.dk>
2017-10-31 14:54:47 +0100

    Removed some testing code from mapping profile Added basic view for category index Added specialized repository

--- DAL/MappingProfile.cs
+++ DAL/MappingProfile.cs
@@ -8,25 +8,13 @@ using System.Threading.Tasks;
 
 namespace ITWEB_Mandatory5.DAL
 {
-    public class A
-    {
-        public int anInt;
-        public float aFloat;
-    }
-
-    public class B
-    {
-        public long aLong;
-        public double aDouble;
-    }
-
-
     public class MappingProfile : Profile
     {
         public MappingProfile()
         {
             // Add as many of these lines as you need to map your objects
 
+            // Category Details
             CreateMap<Category, CategoryDetailsViewmodel>()
                 .ForMember(dest => dest.ComponentTypes, opts => opts.MapFrom(
                     src => src.ComponentTypeCategory.Select(
@@ -34,17 +22,10 @@ namespace ITWEB_Mandatory5.DAL
                     )
                 );
 
+            // Category Index
             CreateMap<IEnumerable<Category>, CategoryIndexViewmodel>()
                 .ForMember(dest => dest.Categories, opts => opts.MapFrom(src => src));
 
-
-            CreateMap<A, B>()
-                .ForMember((dest) => dest.aLong, opts => opts.MapFrom(src => (long)src.anInt))
-                .ForMember((dest) => dest.aDouble, opts => opts.MapFrom(src => (double)src.aFloat));
-
-            CreateMap<B, A>()
-                .ForMember((dest) => dest.anInt, opts => opts.MapFrom(src => (int)src.aLong))
-                .ForMember((dest) => dest.aFloat, opts => opts.MapFrom(src => (double)src.aDouble));
         }
     }
 }
